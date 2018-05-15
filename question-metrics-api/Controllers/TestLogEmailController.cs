5a0609523416b7e6da65918d81452e4e929e74c8
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-05-14 21:33:21 -0300

    Controller para testar envio de email de sink de serilog

---
+++ question-metrics-api/Controllers/TestLogEmailController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using question_metrics_domain;
+using question_metrics_domain.Interfaces;
+using Serilog;
+
+namespace question_metrics_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestLogEmailController : Controller
+    {
+
+        // GET api/values
+        [HttpGet]
+        [Route("Test")]
+        public async Task<IActionResult> Test()
+        {
+            Log.Error("Esta mesnagem de erro deve chegar no e-mail");
+            Log.Warning("Esta mensagem de warning não deve cgear no email");
+
+            return Ok();
+        }
+
+    }
+}
