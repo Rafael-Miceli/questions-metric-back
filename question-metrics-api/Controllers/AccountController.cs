64839a90d50aa957d2988455d2e8d8a2439f4b98
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-05-24 22:21:34 -0300

    Adicionado esquelo de controller para usuario se cadastrar e login

---
+++ question-metrics-api/Controllers/AccountController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace question_metrics_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        
+    }
+}
