23ae77d5dcbf71494c3f273b6ee22f32d07f9d50
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-06-24 21:33:41 -0300

    Inserindo na base Mongo fazendo mapping para DataDto por causa de nao conseguir acessar propriedades privadas

--- question-metrics-data/ExamRepository.cs
+++ question-metrics-data/ExamRepository.cs
@@ -16,19 +16,17 @@ namespace question_metrics_data {
     public class ExamRepository : IExamRepo {
 
         private IMongoDatabase _mongoDb;
-        private IMongoCollection<Exam> _exams { get; set; }
+        private IMongoCollection<ExamDataDto> _exams { get; set; }
         private readonly string _connectionString = string.Empty;
 
         public ExamRepository(IConfiguration configuration) {
             _connectionString = configuration.GetConnectionString("QuestionMetrics");
             InitializeMongoDatabase();
-            _exams = _mongoDb.GetCollection<Exam>("exams");
+            _exams = _mongoDb.GetCollection<ExamDataDto>("exams");
         }
 
-        private void InitializeMongoDatabase()
-        {
-            try
-            {                
+        private void InitializeMongoDatabase() {
+            try {
                 //var client = new MongoClient("mongodb://mongo:27017");
                 //var client = new MongoClient("mongodb://localhost:27017");
 
@@ -40,25 +38,85 @@ namespace question_metrics_data {
 
                 var client = new MongoClient(settings);
                 _mongoDb = client.GetDatabase("QuestionMetrics");
-            }
-            catch (Exception ex) 
-            { 
+            } catch (Exception ex) {
                 Console.WriteLine(ex.Message);
-                _mongoDb = null; 
+                _mongoDb = null;
             }
         }
-        
+
         public async Task<Result> Delete(string examName, DateTime examDate)=> Result.Fail("Não implementado");
 
-        public async Task<IEnumerable<Exam>> GetAll()=> await _exams.Find(_ => true).ToListAsync();
+        public async Task<IEnumerable<Exam>> GetAll()=> (await _exams.Find(_ => true).ToListAsync()).ToExams();
 
         public async Task<Result<Exam>> Insert(Exam exam) {
 
-            await _exams.InsertOneAsync(exam);
+            await _exams.InsertOneAsync(exam.ToExamDataDto());
 
             return Result.Ok(exam);
         }
 
-        public async Task<Exam> GetExamById(string examId)=> await _exams.Find(e => e.Id == examId).FirstOrDefaultAsync();
+        public async Task<Exam> GetExamById(string examId)=> (await _exams.Find(e => e.Id == examId).FirstOrDefaultAsync()).ToExam();
+    }
+
+    public static class DataUtils {
+        public static IEnumerable<Exam> ToExams(this IEnumerable<ExamDataDto> examsData) {
+            return examsData.Select(e => e.ToExam());
+        }
+
+        public static Exam ToExam(this ExamDataDto examData) {
+            return new Exam(
+                examData.Name,
+                examData.Date,
+                examData.Questions.ToQuestions(),
+                examData.Id
+            );
+        }
+
+        public static IEnumerable<Question> ToQuestions(this IEnumerable<QuestionDataDto> questionDataDto) {
+            return questionDataDto.Select(e => e.ToQuestion());
+        }
+
+        public static Question ToQuestion(this QuestionDataDto questionData) {
+            return new Question(
+                questionData.Number,
+                questionData.Answer.ToAnswer()
+            );
+        }
+
+        public static Answer ToAnswer(this AnswerDataDto answerData) {
+            if (answerData.IsAnswerWrong)
+                return new WrongAnswer(ReasonIsWrong.GetReasonIsWrongById(answerData.ReasonIsWrongId));
+            
+            return new CorrectAnswer();
+        }
+
+
+
+        public static ExamDataDto ToExamDataDto(this Exam exam) {
+            return new ExamDataDto{
+                Name = exam.Name,
+                Date = exam.Date,
+                Questions = exam.Questions.ToQuestionsDataDto(),
+                Id = exam.Id
+            };
+        }
+
+        public static IEnumerable<QuestionDataDto> ToQuestionsDataDto(this IEnumerable<Question> question) {
+            return question.Select(e => e.ToQuestionDataDto());
+        }
+
+        public static QuestionDataDto ToQuestionDataDto(this Question questionData) {
+            return new QuestionDataDto {
+                Number = questionData.Number,
+                Answer = questionData.Answer.ToAnswerDataDto()
+            };
+        }
+
+        public static AnswerDataDto ToAnswerDataDto(this Answer answerData) {
+            return new AnswerDataDto {
+                IsAnswerWrong = answerData.IsAnswerWrong,
+                ReasonIsWrongId = answerData.WhyIsWrong == null ? 0 : answerData.WhyIsWrong.Id
+            };
+        }
     }
 }
--- question-metrics-domain/ReasonIsWrong.cs
+++ question-metrics-domain/ReasonIsWrong.cs
@@ -1,21 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace question_metrics_domain
 {
     public class ReasonIsWrong
     {
-        public static ReasonIsWrong DidntReadProperlyAnswer = new ReasonIsWrong("Me Confundi na leitura da resposta.");
-        public static ReasonIsWrong DidntReadProperlyQuestion = new ReasonIsWrong("Me Confundi na leitura da pergunta.");
-        public static ReasonIsWrong NotAnswered = new ReasonIsWrong("Não respondida.");
-        public static ReasonIsWrong NotSpecified = new ReasonIsWrong("Não especificado porque errei. Isto não ajuda muito.");
-        public static ReasonIsWrong ForgetSubject = new ReasonIsWrong("Esqueci o asunto da matéria.");
+        public static ReasonIsWrong DidntReadProperlyAnswer = new ReasonIsWrong(1, "Me Confundi na leitura da resposta.");
+        public static ReasonIsWrong DidntReadProperlyQuestion = new ReasonIsWrong(2, "Me Confundi na leitura da pergunta.");
+        public static ReasonIsWrong NotAnswered = new ReasonIsWrong(3, "Não respondida.");
+        public static ReasonIsWrong NotSpecified = new ReasonIsWrong(4, "Não especificado porque errei. Isto não ajuda muito.");
+        public static ReasonIsWrong ForgetSubject = new ReasonIsWrong(5, "Esqueci o asunto da matéria.");
 
         //public static ReasonIsWrong CorrectAnswer = new ReasonIsWrong("");
 
-        private ReasonIsWrong(string reason)
+        private static List<ReasonIsWrong> _reasonsCanBeWrong = new List<ReasonIsWrong> {
+            DidntReadProperlyAnswer,
+            DidntReadProperlyQuestion,
+            NotAnswered,
+            NotSpecified,
+            ForgetSubject
+        };
+
+        public static ReasonIsWrong GetReasonIsWrongById(int id) => _reasonsCanBeWrong.FirstOrDefault(r => r.Id == id);
+
+        private ReasonIsWrong(int id, string reason)
         {
+            Id = id;
             Reason = reason;
         }
 
+        public int Id { get; }
         public string Reason {get;}
+
+
     }
 }
 
